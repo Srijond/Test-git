796a03f0b8ce3baf5492cb5599b9244fc790d823
srijond <srijonrahman00@gmail.com>
2019-03-03 12:13:09 +0600

    3 triangle check

--- esi.c
+++ esi.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+//triangle check equilateral or scalene or isosceles...
+int main()
+{
+   int side1,side2,side3;
+   scanf("%d %d %d",&side1,&side2,&side3);
+   if(side1==side2 && side2==side3){
+    printf("triangle is equilateral\n");
 
+   }
+   else if(side1==side2||side2==side3||side1==side3){
+    printf("triangle is isosceles\n");
+   }
+   else{
+    printf("triangle is scalene\n");
+   }
+
+  return 0;
+
+}
